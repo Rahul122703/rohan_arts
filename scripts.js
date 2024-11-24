bd518d23bb89472ef499b64c9728b1a0d4a26eca
rahul122703 <killkamam3@gmail.com>
2024-11-24 23:32:01 +0530

    added vide to the bg and also side bar

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const toggleBtn = document.getElementById("toggleSidebar");
+const sidebar = document.getElementById("sidebar");
+const closeBtn = document.getElementById("closeBtn");
+
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.add("active");
+  toggleBtn.style.display = "none";
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  toggleBtn.style.display = "block";
+});
